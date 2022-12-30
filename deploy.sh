0ea9760690e7bfead65bbad830188f1530c93e01
Gabriel <gabrieldsfs@gmail.com>
2022-12-30 14:19:44 +0400

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:russ0133/portfolio.git main:gh-pages
+git push -f git@github.com:russ0133/portfolio main:gh-pages
 
 cd -
